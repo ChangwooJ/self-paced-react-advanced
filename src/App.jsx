15cc47e9a87238df8783a654932e889050da947a
Yeeun Park <ye6194@naver.com>
2024-11-19 14:37:41 +0900

    feat: html 템플릿 App.jsx에서 그리기

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-
-import './App.css'
+import "./App.css";
+import Header from "./components/Header.jsx";
+import CategoryFilter from "./components/CategoryFilter.jsx";
+import RestaurantList from "./components/RestaurantList.jsx";
+import AddRestaurantModal from "./components/AddRestaurantModal.jsx";
+import RestaurantDetailModal from "./components/RestaurantDetailModal.jsx";
 
 function App() {
   return (
     <>
-        Code It..
+      <header>
+        <Header />
+      </header>
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
